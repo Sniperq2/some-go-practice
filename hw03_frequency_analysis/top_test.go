5af16ce5b59b8ee154f420d09b928a86534d602f
Sniperq2 <ogldelphi@mail.ru>
2022-10-08 01:01:47 +0300

    Add one more test

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -52,6 +52,10 @@ func TestTop10(t *testing.T) {
 		require.Len(t, Top10(text), 10)
 	})
 
+	t.Run("returned slice of string", func(t *testing.T) {
+		require.IsType(t, []string{}, Top10(text))
+	})
+
 	t.Run("positive test", func(t *testing.T) {
 		if taskWithAsteriskIsCompleted {
 			expected := []string{
