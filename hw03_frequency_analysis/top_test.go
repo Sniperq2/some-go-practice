dbfc89ec8d971c6f7bbb600581c0d4b86c252262
Sniperq2 <ogldelphi@mail.ru>
2022-10-08 00:56:22 +0300

    Add test cover 100%

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -48,6 +48,10 @@ func TestTop10(t *testing.T) {
 		require.Len(t, Top10(""), 0)
 	})
 
+	t.Run("len 10 must returned", func(t *testing.T) {
+		require.Len(t, Top10(text), 10)
+	})
+
 	t.Run("positive test", func(t *testing.T) {
 		if taskWithAsteriskIsCompleted {
 			expected := []string{
