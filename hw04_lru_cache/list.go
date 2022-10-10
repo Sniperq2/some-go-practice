19715eaa601d658b43e39b3173b187f3f4101591
Sniperq2 <ogldelphi@mail.ru>
2022-10-10 22:26:30 +0300

    HW04 WIP HEED HELP

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,59 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	List
+	Head *ListItem
+	Tail *ListItem
+	len  int
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.Head // вернём "голову"
+}
+
+func (l *list) Back() *ListItem {
+	return l.Tail // вернем "хвост"
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	n := &ListItem{Value: v}
+
+	if l.Head == nil { // если "головы" нет, значит и списка нет
+		l.Head = n // добавим новый элемент к списку
+	} else { // если есть "голова" то
+		temp := l.Head     // сохраняем предыдущую "голову" в temp
+		l.Head = n         // новый элемент становится "головой" списка
+		n.Next = temp.Prev // меняем указатель prev на next нового списка
+	}
+	l.len++ // увеличиваем счетчик длины списка
+	return l.Head
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	n := &ListItem{Value: v}
+	if l.Head == nil { // если "головы" нет, значит и списка нет
+		l.Head = n // добавим новый элемент к списку
+	} else { // если есть "голова" то
+		l.Tail.Next = n // новый элемент пристегнем к хвосту next
+	}
+
+	l.len++ // увеличиваем счетчик длины списка
+
+	return l.Tail
+}
+
+func (l *list) Remove(i *ListItem) {
+
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+
 }
