1625c44d7657dffb4b96f1a1c6312b29395f6742
Sniperq2 <ogldelphi@mail.ru>
2022-10-17 22:27:08 +0300

    HW05 WIP

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,24 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var wg sync.WaitGroup
+
+	numberOfTasks := len(tasks)
+	t := make(chan Task, numberOfTasks)
+
+	for i := 0; i < n; i++ { // создаем n горутин
+		wg.Add(1)
+		go func(t <-chan Task, wg *sync.WaitGroup, m int) {
+			defer wg.Done()
+			fmt.Println(<-t) // читаем из канала и "выполняем" таску
+			// как-то тут надо ошибки считать и передавать в m
+		}(t, &wg, m)
+	}
+
+	for k := 0; k < numberOfTasks; k++ {
+		t <- tasks[k]
+	}
+	close(t)  // закрываем канал
+	wg.Wait() // ждем пока выполнятся все таски
 	return nil
 }
