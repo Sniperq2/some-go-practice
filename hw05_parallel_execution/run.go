97299cd74de7b8880ee199a09ce3fd0c9e579d9f
Sniperq2 <ogldelphi@mail.ru>
2022-11-05 20:48:21 +0300

    Add full logic but there is an error somewhere

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,7 +2,6 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"fmt"
 	"sync"
 )
 
@@ -16,20 +15,52 @@ func Run(tasks []Task, n, m int) error {
 
 	numberOfTasks := len(tasks)
 	t := make(chan Task, numberOfTasks)
-
-	for i := 0; i < n; i++ { // создаем n горутин
+	errChannel := make(chan int)  // канал для записи ошибок
+	exitChannel := make(chan int) // канал для выхода из программы если в него что-то записали
+	for i := 0; i < n; i++ {      // создаем n горутин
 		wg.Add(1)
-		go func(t <-chan Task, wg *sync.WaitGroup, m int) {
-			defer wg.Done()
-			fmt.Println(<-t) // читаем из канала и "выполняем" таску
-			// как-то тут надо ошибки считать и передавать в m
-		}(t, &wg, m)
+		go func(t <-chan Task, errChannel chan<- int, exitChannel <-chan int) {
+			defer wg.Done() // если горутина выполнилась
+
+			for {
+				select {
+				case result := <-t: // читаем из канала новую задачу
+					err := result() // выполняем
+					if err != nil { // если ошибка
+						select {
+						case errChannel <- 1: //то пишем в канал ошибок
+							err = nil
+						case <-exitChannel: // читаем канал если есть что-то - значит выходим
+							return
+						}
+					}
+				case <-exitChannel: // читаем канал если есть что-то - значит выходим
+					return
+				}
+			}
+		}(t, errChannel, exitChannel)
 	}
 
+	var errorsCount int
+	var count int
 	for k := 0; k < numberOfTasks; k++ {
-		t <- tasks[k]
+		result := tasks[k]
+		select {
+		case <-errChannel: //читаем из канала ошибок
+			errorsCount++
+		case t <- result:
+			count++
+		}
 	}
-	close(t)  // закрываем канал
+
+	close(exitChannel)
 	wg.Wait() // ждем пока выполнятся все таски
+	close(t)  // а уже потом закрываем канал
+	close(errChannel)
+
+	if errorsCount >= m {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
