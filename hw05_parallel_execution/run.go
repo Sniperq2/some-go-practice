b19eaf588fa1715d7508d74a0cf06385f6448d26
Sniperq2 <ogldelphi@mail.ru>
2022-11-06 22:00:52 +0300

    Fix all suggestions but do not works

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -13,49 +13,36 @@ type Task func() error
 func Run(tasks []Task, n, m int) error {
 	var wg sync.WaitGroup
 
-	numberOfTasks := len(tasks)
-	t := make(chan Task, numberOfTasks)
-	errChannel := make(chan int)  // канал для записи ошибок
-	exitChannel := make(chan int) // канал для выхода из программы если в него что-то записали
-	for i := 0; i < n; i++ {      // создаем n горутин
+	t := make(chan Task)
+	errChannel := make(chan struct{}) // канал для записи ошибок
+	for i := 0; i < n; i++ {          // создаем n горутин
 		wg.Add(1)
-		go func(t <-chan Task, errChannel chan<- int, exitChannel <-chan int) {
+		go func(t <-chan Task, errChannel chan<- struct{}) {
 			defer wg.Done() // если горутина выполнилась
 
-			for {
-				select {
-				case result := <-t: // читаем из канала новую задачу
-					err := result() // выполняем
-					if err != nil { // если ошибка
-						select {
-						case errChannel <- 1: //то пишем в канал ошибок
-							err = nil
-						case <-exitChannel: // читаем канал если есть что-то - значит выходим
-							return
-						}
-					}
-				case <-exitChannel: // читаем канал если есть что-то - значит выходим
-					return
+			for result := range t { // в цикле по каналу читаем новую задачу
+				if err := result(); err != nil { // выполняем и если ошибка
+					errChannel <- struct{}{} // пишем в канал сигнал об ошибке
 				}
 			}
-		}(t, errChannel, exitChannel)
+		}(t, errChannel)
 	}
 
 	var errorsCount int
 	var count int
-	for k := 0; k < numberOfTasks; k++ {
-		result := tasks[k]
+	for _, result := range tasks {
+		if errorsCount >= m { // если ошибок больше чем допустимо выходим из цикла
+			break
+		}
 		select {
-		case <-errChannel: //читаем из канала ошибок
+		case <-errChannel: // читаем из канала ошибок
 			errorsCount++
 		case t <- result:
 			count++
 		}
 	}
 
-	close(exitChannel)
 	wg.Wait() // ждем пока выполнятся все таски
-	close(t)  // а уже потом закрываем канал
 	close(errChannel)
 
 	if errorsCount >= m {
