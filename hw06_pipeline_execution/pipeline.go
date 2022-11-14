b9c075e02ba44996eebc1e3f0af7a2c7c814078a
Sniperq2 <ogldelphi@mail.ru>
2022-11-14 22:41:14 +0300

    WIP

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var out Out = in
+	for _, stage := range stages { // для каждого стейджа
+		makeBi := make(Bi) // создадим промежуточный канал
+		// if ind == 0 { // если первый раз запустили то нет out и надо передать in
+		//	out = in
+		//}
+
+		// in ---------|
+		//  ^         out
+		//  |          |
+		//  ----------<-
+
+		go func(Out) {
+			defer close(makeBi)
+			for {
+				select {
+				case <-done: // прерываем канал по done
+					return
+				case v, ok := <-in: // читаем из канала in
+					if !ok { // если на входе пусто то и делать нечего
+						return
+					}
+					makeBi <- v // в промежуточный канал
+				}
+			}
+		}(out) // первый раз в out будет in
+
+		out = stage(makeBi) // делаем полезную работу в стейдже
+	}
+
+	return out
 }
