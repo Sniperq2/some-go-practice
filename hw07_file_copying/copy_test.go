c83de5b4408e1291c7b180ef795a23585cd6e2cc
Sniperq2 <ogldelphi@mail.ru>
2022-11-23 22:14:24 +0300

    Add more tests

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCopy(_ *testing.T) {
-	Copy("./testdata/input.txt", "out.txt", 0, 100)
+func TestCopyFine(t *testing.T) {
+	err := Copy("./testdata/input.txt", "out.txt", 0, 100)
+	assert.Nil(t, err, "must copying fine")
+}
+
+func TestCopyFileSizeIsGreater(t *testing.T) {
+	if err := Copy("./testdata/input.txt", "out.txt", 7000, 100); err != nil {
+		assert.ErrorContains(t, err, "offset exceeds file size", "file size is greater then offset")
+	}
+}
+
+func TestCopyForUnsupportedFiles(t *testing.T) {
+	if err := Copy("C:\\msys64\\dev\\stderr", "out.txt", 0, 100); err != nil {
+		assert.ErrorContains(t, err, "unsupported file", "unsupported file test")
+	}
 }
