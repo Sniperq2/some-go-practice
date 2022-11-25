b08bcacd7044091fffc5b1548a0c91087ac5dba9
Sniperq2 <ogldelphi@mail.ru>
2022-11-25 20:20:02 +0300

    Get pipeline function out

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,6 +8,21 @@ type (
 
 type Stage func(in In) (out Out)
 
+func pipeline(in In, done In, Bi chan interface{}) {
+	defer close(Bi)
+	for {
+		select {
+		case v, ok := <-in: // читаем из канала in
+			if !ok { // если на входе пусто то и делать нечего
+				return
+			}
+			Bi <- v // в промежуточный канал
+		case <-done: // прерываем канал по done
+			return
+		}
+	}
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	out := in
 	for _, stage := range stages { // для каждого стейджа
@@ -21,20 +36,7 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 		//  |          |
 		//  ----------<-
 
-		go func(Out) {
-			defer close(makeBi)
-			for {
-				select {
-				case <-done: // прерываем канал по done
-					return
-				case v, ok := <-in: // читаем из канала in
-					if !ok { // если на входе пусто то и делать нечего
-						return
-					}
-					makeBi <- v // в промежуточный канал
-				}
-			}
-		}(out) // первый раз в out будет in
+		go pipeline(out, done, makeBi) // первый раз в out будет in
 
 		out = stage(makeBi) // делаем полезную работу в стейдже
 	}
