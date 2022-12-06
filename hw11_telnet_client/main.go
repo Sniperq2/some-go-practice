21af416ad8f0c58a928adb86ff181d940c42baeb
Sniperq2 <ogldelphi@mail.ru>
2022-12-06 10:47:10 +0300

    Add simple flag support

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	flag "github.com/spf13/pflag"
+)
+
+var (
+	timeout string
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.StringVar(&timeout, "timeout", "10s", "timeout")
+	flag.Parse()
+	host := os.Args[2]
+	port := os.Args[3]
+	if len(host) == 0 {
+		log.Fatalf("Please set host")
+	}
+	if len(port) == 0 {
+		log.Fatalf("Please set port")
+	}
+
+	fmt.Printf("%s %s %s", timeout, host, port)
 }
