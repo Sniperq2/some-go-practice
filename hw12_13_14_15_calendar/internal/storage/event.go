667ca0f8b01b1bc68d5de08d824472c7d9485314
Sniperq2 <ogldelphi@mail.ru>
2022-12-10 13:40:17 +0300

    Start implementing storage package

--- hw12_13_14_15_calendar/internal/storage/event.go
+++ hw12_13_14_15_calendar/internal/storage/event.go
@@ -1,7 +1,31 @@
 package storage
 
+type EventInterface interface {
+	Create() bool
+	Edit(int) int
+	Delete(int) bool
+}
+
 type Event struct {
 	ID    string
 	Title string
 	// TODO
+}
+
+type EventList *[]Event
+
+func NewEvent(id, title string) EventInterface {
+	return &Event{id, title}
+}
+
+func (e *Event) Edit(int) int {
+	return 0
+}
+
+func (e *Event) Delete(int) bool {
+	return true
+}
+
+func (e *Event) Create() bool {
+	return true
 }
