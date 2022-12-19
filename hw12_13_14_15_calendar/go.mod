f9768cbb7c87de3669efdb8f5edd99978eafaa56
Sniperq2 <ogldelphi@mail.ru>
2022-12-19 21:40:09 +0300

    Simple zerolog configuration

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -1,3 +1,5 @@
 module github.com/fixme_my_friend/hw12_13_14_15_calendar
 
 go 1.16
+
+require github.com/rs/zerolog v1.28.0
--- hw12_13_14_15_calendar/internal/logger/logger.go
+++ hw12_13_14_15_calendar/internal/logger/logger.go
@@ -1,20 +1,39 @@
 package logger
 
-import "fmt"
+import (
+	"io"
 
-type Logger struct { // TODO
+	"github.com/rs/zerolog"
+)
+
+type LoggerIntface interface {
+	Debug(message string)
+	Info(message string)
+	Warn(message string)
+	Error(message string)
 }
 
-func New(level string) *Logger {
-	return &Logger{}
+type Logger struct {
+	logger *zerolog.Logger
 }
 
-func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+func New(logWriter io.Writer, level string) *Logger {
+	logger := zerolog.New(logWriter).With().Logger()
+	return &Logger{logger: &logger}
 }
 
-func (l Logger) Error(msg string) {
-	// TODO
+func (l *Logger) Info(msg string) {
+	l.logger.Info().Msg(msg)
 }
 
-// TODO
+func (l *Logger) Error(msg string) {
+	l.logger.Error().Msg(msg)
+}
+
+func (l *Logger) Debug(msg string) {
+	l.logger.Debug().Msg(msg)
+}
+
+func (l *Logger) Warning(msg string) {
+	l.logger.Warn().Msg(msg)
+}
