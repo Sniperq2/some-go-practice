de82cdbe29ac785c62d8b7d3847ed5e6d996cab0
Sniperq2 <ogldelphi@mail.ru>
2022-12-26 23:31:51 +0300

    Do not store users, just count domains

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -9,56 +9,36 @@ import (
 )
 
 type User struct {
-	ID       int    `json:"-"`
-	Name     string `json:"-"`
-	Username string `json:"-"`
+	ID       int
+	Name     string
+	Username string
 	Email    string
-	Phone    string `json:"-"`
-	Password string `json:"-"`
-	Address  string `json:"-"`
+	Phone    string
+	Password string
+	Address  string
 }
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
+	domainName := fmt.Sprintf(".%s", domain)
 	scanner := bufio.NewScanner(r)
-	counter := 0
+	result := make(DomainStat)
 	for scanner.Scan() {
 		var user User
-		if err = json.Unmarshal(scanner.Bytes(), &user); err != nil {
-			return
+		if err := json.Unmarshal(scanner.Bytes(), &user); err != nil {
+			return nil, err
 		}
-		result[counter] = user
-		counter++
-	}
-
-	if err = scanner.Err(); err != nil {
-		return
-	}
-
-	return
-}
-
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	domainName := fmt.Sprintf(".%s", domain)
-	for _, user := range u {
 		if strings.HasSuffix(user.Email, domainName) {
 			lowerEmail := strings.ToLower(user.Email)
 			splittedEmail := strings.SplitN(lowerEmail, "@", 2)
 			result[splittedEmail[1]]++
 		}
 	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
 	return result, nil
 }
