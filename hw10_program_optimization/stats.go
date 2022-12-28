feedd8f19ad09bf516c953c1e9491c241c960a4c
Sniperq2 <ogldelphi@mail.ru>
2022-12-29 00:22:37 +0300

    Final fix

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -22,7 +22,7 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	var splittedEmail []string
 	var err error
 	var userEmail string
-	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
 	for scanner.Scan() {
 		if err = json.Unmarshal(scanner.Bytes(), &user); err != nil {
 			return nil, err
